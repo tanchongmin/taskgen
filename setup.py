59fdf9e68702044cc0deb3d1a656c0b6dbd8c52d
tanchongmin <46222748+tanchongmin@users.noreply.github.com>
2024-05-09 10:34:29 +0800

    Add files via upload
    v2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="taskgen",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     install_requires=[
         "openai>=1.3.6",
