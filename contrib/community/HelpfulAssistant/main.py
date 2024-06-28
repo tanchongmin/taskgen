dc723f3c91a5a6b0f6f226e3a7ea1f68b95c781b
tanchongmin <46222748+tanchongmin@users.noreply.github.com>
2024-06-28 14:16:23 +0800

    Update main.py

--- contrib/community/HelpfulAssistant/main.py
+++ contrib/community/HelpfulAssistant/main.py
@@ -26,15 +26,15 @@ class HelpfulAssistant(Agent):
         super().__init__(
             agent_name="Helpful assistant",
             agent_description='''You are a generalist agent''',
-            max_subtasks="5",
-            summarise_subtasks_count="5",
+            max_subtasks=5,
+            summarise_subtasks_count=5,
             memory_bank={'Function': Memory(memory=[], top_k=5, mapper=lambda x: x.fn_name + ': ' + x.fn_description, approach='retrieve_by_ranker', ranker=Ranker(model='text-embedding-3-small', ranking_fn=None)),},
             shared_variables={},
             get_global_context=None,
             global_context='''''',
-            default_to_llm="True",
-            verbose="True",
-            debug="False"
+            default_to_llm=True,
+            verbose=True,
+            debug=False
         )
 
         self.assign_functions(
